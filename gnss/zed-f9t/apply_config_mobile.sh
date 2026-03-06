447c5f24da4f26eff0f7d7ab057b53bd611df767
Anil Gurses <anilgurses98@gmail.com>
2026-03-05 21:52:32 -0500

    Add zed f9t ubx config

---
+++ gnss/zed-f9t/apply_config_mobile.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# This is for mobile node
+set -e
+
+DEV="/dev/ttyACM0"
+
+echo "Reset receiver to defaults"
+ubxtool -f $DEV -p CFG-CFG,0xffff,0xffff,0
+sleep 2
+ubxtool -f $DEV -p RESET
+sleep 3
+
+# It shouldn't be on survey in mode
+ubxtool -f $DEV -z CFG-TMODE-MODE,0
+
+# PPS configuration
+ubxtool -f $DEV -z CFG-TP-TP1_ENA,1
+ubxtool -f $DEV -z CFG-TP-SYNC_GNSS_TP1,1
+ubxtool -f $DEV -z CFG-TP-USE_LOCKED_TP1,1
+ubxtool -f $DEV -z CFG-TP-ALIGN_TO_TOW_TP1,1
+
+ubxtool -f $DEV -z CFG-TP-PERIOD_TP1,1000000
+ubxtool -f $DEV -z CFG-TP-PERIOD_LOCK_TP1,1000000
+ubxtool -f $DEV -z CFG-TP-LEN_TP1,100000
+ubxtool -f $DEV -z CFG-TP-LEN_LOCK_TP1,100000
+
+# UART RTCM mode
+ubxtool -f $DEV -z CFG-UART1-BAUDRATE,115200
+ubxtool -f $DEV -z CFG-UART1INPROT-RTCM3X,1
+ubxtool -f $DEV -z CFG-UART1INPROT-UBX,1
+
+# Enable NMEA + UBX
+ubxtool -f $DEV -z CFG-USBOUTPROT-NMEA,1
+ubxtool -f $DEV -z CFG-USBOUTPROT-UBX,1
+ubxtool -f $DEV -z CFG-UART1OUTPROT-NMEA,1
+ubxtool -f $DEV -z CFG-MSGOUT-NMEA_ID_GGA_USB,1
+ubxtool -f $DEV -z CFG-MSGOUT-NMEA_ID_RMC_USB,1
+ubxtool -f $DEV -z CFG-MSGOUT-NMEA_ID_GSA_USB,1
+ubxtool -f $DEV -z CFG-MSGOUT-NMEA_ID_GSV_USB,1
+
+# Save to flash
+ubxtool -f $DEV -p SAVE
+
+echo "Configuration complete."
